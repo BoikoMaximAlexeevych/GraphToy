82263a2300399db3f5bc7d1383d9f38ff91321f9
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2025-07-30 16:27:33 +0300

    рефакторинг вершины
    и попытки в интерфейс

--- g_node.gd
+++ g_node.gd
@@ -1,27 +1,29 @@
 extends Node2D
 class_name Gnode
 
+# Components
+@export var adjacencyComponent: AdjacencyComponent
+
+@onready var repel_area = $repelArea
+
+#Constants
+@export var centerForceCoef: float
+@export var repelForceCoef: float
+@export var linkForceCoef: float
+
 var velocity: Vector2
 var center_force: Vector2
 var repel_force: Vector2
 var link_force: Vector2
 var min_dist: float = 100
 
-@onready var repel_area = $repelArea
-@export var adjacencyComponent: AdjacencyComponent
-
-func _ready() -> void:
-	set_meta("connected_nodes", [])
-	
 func calculate_repel() -> Vector2:
 	var near = repel_area.get_overlapping_areas()
-	var size = near.size()
 	var res: Vector2
 	for peer in near:
 		var direction: Vector2 = peer.global_position - self.global_position
-		var length = direction.length()
-		res -= direction / (length / 50 )
-	return res
+		res -= direction 
+	return res.normalized()
 
 var prevvel: Vector2
 func _process(delta: float) -> void:
@@ -29,12 +31,12 @@ func _process(delta: float) -> void:
 	var peers = adjacencyComponent.adjacent_nodes
 	if peers.size() > 0:
 		for peer in peers:
-			link_force = (peer.position - self.position)
+			link_force = (peer.position - self.position).normalized()
 			if((peer.position - self.position).length() <= min_dist):
 				link_force *= -1.5
-				
-	center_force = (get_viewport_rect().get_center() - self.position)
+
+	center_force = (get_viewport_rect().get_center() - self.position).normalized()
 	repel_force = calculate_repel() 
-	velocity = (link_force * 0.2 + repel_force * 0.6 + center_force) 
+	velocity = (link_force * linkForceCoef + repel_force * repelForceCoef + center_force * centerForceCoef) 
 	velocity = (prevvel + velocity) / 2
 	position += velocity * delta
